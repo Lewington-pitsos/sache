c550f74b8b90264049a4ce3eb4c4ed6b855338a1
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-09-18 09:33:18 +1000

    create non distributed download script

--- scripts/laion.py
+++ scripts/laion.py
@@ -1,1 +1,28 @@
-# for i in {00000..00127}; do wget --header="Authorization: Bearer XXX" https://huggingface.co/datasets/laion/laion2B-multi-joined-translated-to-en/resolve/main/.part-$i-00478b7a-941e-4176-b569-25f4be656991-c000.snappy.parquet.crc; done
+from img2dataset import download
+import shutil
+import os
+from pyspark.sql import SparkSession  # pylint: disable=import-outside-toplevel
+
+output_dir = os.path.abspath("bench")
+
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+
+spark = (
+    SparkSession.builder.config("spark.driver.memory", "16G").master("local[16]").appName("spark-stats").getOrCreate()
+)
+
+download(
+    processes_count=12,
+    thread_count=32,
+    url_list="../tests/test_files/test_10000.parquet",
+    image_size=256,
+    output_folder=output_dir,
+    output_format="webdataset",
+    input_format="parquet",
+    url_col="URL",
+    caption_col="TEXT",
+    enable_wandb=True,
+    number_sample_per_shard=1000,
+    distributor="pyspark",
+)

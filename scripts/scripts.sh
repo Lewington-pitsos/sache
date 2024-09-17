bb763aa063d1c80b92261898cb30b70d1e10b386
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2024-09-17 23:38:44 +0000

    add scripts for downloading laion

---
+++ scripts/scripts.sh
@@ -0,0 +1,4 @@
+for i in {00000..00127}; do wget --header="Authorization: Bearer $HF_TOKEN" https://huggingface.co/datasets/laion/laion2B-multi-joined-translated-to-en/resolve/main/.part-$i-00478b7a-941e-4176-b569-25f4be656991-c000.snappy.parquet.crc; done
+
+
+for i in {00000..00010}; do wget --header="Authorization: Bearer $HF_TOKEN" https://huggingface.co/datasets/laion/laion2B-multi-joined-translated-to-en/resolve/main/.part-$i-00478b7a-941e-4176-b569-25f4be656991-c000.snappy.parquet.crc; done

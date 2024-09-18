21662c489c099f71bf1593a600a4188b29d65544
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2024-09-18 01:18:13 +0000

    load files

--- scripts/laion.py
+++ scripts/laion.py
@@ -3,26 +3,21 @@ import shutil
 import os
 from pyspark.sql import SparkSession  # pylint: disable=import-outside-toplevel
 
-output_dir = os.path.abspath("laion_imgs")
+if __name__ == "__main__":
+    spark = (
+        SparkSession.builder.config("spark.driver.memory", "32G").master("local[16]").appName("spark-stats").getOrCreate()
+    )
 
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir)
-
-spark = (
-    SparkSession.builder.config("spark.driver.memory", "32G").master("local[16]").appName("spark-stats").getOrCreate()
-)
-
-download(
-    processes_count=12,
-    thread_count=32,
-    url_list="laion/pq-1/",
-    image_size=224, # as used by https://huggingface.co/laion/CLIP-ViT-L-14-laion2B-s32B-b82K/tree/main
-    output_folder=output_dir,
-    output_format="webdataset",
-    input_format="parquet",
-    url_col="URL",
-    caption_col="TEXT",
-    enable_wandb=True,
-    distributor="pyspark",
-    min_image_size=64,
-)
+    download(
+        processes_count=12,
+        thread_count=32,
+        url_list="laion/pq-1/",
+        image_size=224, # as used by https://huggingface.co/laion/CLIP-ViT-L-14-laion2B-s32B-b82K/tree/main
+        output_format="files",
+        input_format="parquet",
+        url_col="URL",
+        caption_col="TEXT",
+        enable_wandb=False,
+        distributor="pyspark",
+        min_image_size=64,
+    )

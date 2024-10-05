72c9fc7bd0b92d53afff8d212b6e7ea2bba79eee
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-10-05 11:22:23 +1000

    add img2dataset dependency

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -5,3 +5,7 @@ module "sache_production" {
 
 
 # "g6e.4xlarge" # 20 Gbps bandwidth = 2500 MBps, 48 GPU ram, 16 VCPU
+# 625 MBps on a g6e.2xlarge instance
+# (8 layer * 50 * 257 * 1024) / 1024 / 1024 = 100 MBps requried @ 50 images per second
+
+# 10000000 / 50 /60/60 = 55 hours @ 50 images per second

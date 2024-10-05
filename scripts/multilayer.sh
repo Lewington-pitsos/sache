9984e8e595d46e15843e826a5d2e0eb8f48db12e
Ubuntu <ubuntu@ip-172-31-17-162.ec2.internal>
2024-10-05 02:41:22 +0000

    manually set workers

--- scripts/multilayer.sh
+++ scripts/multilayer.sh
@@ -1,1 +1,1 @@
-python scripts/vit_generate.py --run_name "multilayer" --n_samples=20000 --batch_size=1024 --batches_per_cache=11 --full_sequence --input_tensor_shape="(257,1024)" --bucket_name='lewington-pitsos-sache' --log_every=2 
+python scripts/vit_generate.py --run_name "multilayer" --n_samples=20000 --batch_size=1024 --batches_per_cache=11 --full_sequence --input_tensor_shape="(257,1024)" --bucket_name='lewington-pitsos-sache' --log_every=2 --num_cache_workers=5 --num_data_workers=2

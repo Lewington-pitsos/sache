7a2de13f0727643bc3ae7cdda34d12cd1fad5e76
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-10-16 10:01:41 +1100

    export

--- sache/__init__.py
+++ sache/__init__.py
@@ -1,3 +1,4 @@
 from sache.generator import vit_generate
 from sache.train import train_sae, find_s3_checkpoint, save_sae_checkpoint, load_sae_checkpoint
 from sache.hookedvit import SpecifiedHookedViT
+from sache.model import SAE, TopKSAE, TopKSwitchSAE, SwitchSAE
